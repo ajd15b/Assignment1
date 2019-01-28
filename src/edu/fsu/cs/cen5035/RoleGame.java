e74dce3e1a5336126a14994e8ceade8c61b45cc3
alex <alexjdupper@gmail.com>
2019-01-28 15:22:09 -0500

    Added test for Simple Hammer
    Added test for simple hammer and removed typos

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -12,13 +12,16 @@ public class RoleGame {
 
         Weapon weapon = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + weapon.hit() + " of damage.");        
-        System.out.println("Swords was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        System.out.println("Sword was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
         weapon = WeaponFactory.getWeapon("spear");
         System.out.println("Spear has " + weapon.hit() + " of damage.");        
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
-        //TODO: Add the remainig weapons here
+        //TODO: Add the remaining weapons here
+        weapon = WeaponFactory.getWeapon("simple_hammer");
+        System.out.println("Simple Hammer has " + weapon.hit() + " of damage.");
+        System.out.println("Simple Hammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
     }
 }
